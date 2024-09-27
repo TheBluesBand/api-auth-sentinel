02a8cc3880ebf3b221070d82ad7b4ae4e756b17f
Jake McCoy <the.mccoy@outlook.com>
2024-09-27 18:21:51 +1000

    Added a console console log to log all calls that hit the expressJS server for debugging

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import router from "./routes"; // Import the router from the routes module
 const app: express.Application = express(); // Create an instance of an Express application
 const port: number = 3000; // Define the port number on which the server will listen
 
+// Middleware to log incoming API calls
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(`Received ${req.method} request for ${req.url}`);
+    next();
+  }
+);
+
 app.use("/", router); // Use the imported router for handling routes starting from the root path
 
 app.listen(port, () => {
